62c12049935de7163583031d5dff9c49a6bb240d
js4679 <63320565+js4679@users.noreply.github.com>
2020-11-17 14:43:40 -0500

    background start

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,8 +1,8 @@
-import {bubbleAnimation} from "./demo.js"
-// import {loaderAnimation} from "./demo.js"
+import {backgroundAnimation} from "./demo.js"
+
 import {gsap} from "gsap";
 
 const mainTL = gsap.timeline();
-mainTL.add(bubbleAnimation());
-    // .add(loaderAnimation());
+mainTL.add(backgroundAnimation());
+    
 
