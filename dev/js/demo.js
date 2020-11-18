d05382f3f13730767719e1d944d7fee11036aa94
js4679 <63320565+js4679@users.noreply.github.com>
2020-11-18 10:49:03 -0500

    background working

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -3,8 +3,7 @@ import {gsap} from "gsap";
 
 
 const backgroundTL =gsap.timeline();
-backgroundTL.from("#background", {duration:1, fill:"000"})
-            .to("#background", {duration:1, fill:"272727"});
+backgroundTL.to("#background", {duration:7, alpha: 0});
 
 
 export function backgroundAnimation(){
