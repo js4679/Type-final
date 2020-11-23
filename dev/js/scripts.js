8aef47ba23ffb8fb7d64177c53067bd69412c322
js4679 <63320565+js4679@users.noreply.github.com>
2020-11-23 17:27:55 -0500

    before class

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,12 +1,13 @@
-// import {backgroundAnimation} from "./demo.js"
+import {backgroundAnimation} from "./demo.js"
 import {swansAnimation} from "./swans.js"
 import {quadAnimation} from "./quad.js"
 
 import {gsap} from "gsap";
 
 const mainTL = gsap.timeline();
-// mainTL.add(backgroundAnimation())
-        mainTL.add(swansAnimation())
-        .add(quadAnimation());
+
+mainTL.add(backgroundAnimation(), "play")
+        .add(swansAnimation())
+        .add(quadAnimation(),"play");
     
 
