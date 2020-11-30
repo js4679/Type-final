ba794f66acf84e9bfa88be16cb918c0cf1b78143
js4679 <63320565+js4679@users.noreply.github.com>
2020-11-30 10:56:24 -0500

    placement done

--- dev/js/quad.js
+++ dev/js/quad.js
@@ -28,17 +28,17 @@ export function quadAnimation(){
         .to("#gunk-u",{duration: 1, morphSVG:"#swan-w", x: 210, y: -250}, "w")
         .to("#gunk-n",{duration: 1, morphSVG:"#swan-a", x: 220, y: -250}, "a")
         .to("#gunk-k",{duration: 1, morphSVG:"#swan-n", x: 230, y: -250}, "n")
-        .to("#reek-r",{duration: 1, morphSVG:"#swan-s", x: 210, y: 330}, "s")
-        .to("#reek-e-1",{duration: 1, morphSVG:"#swan-w", x: 220, y: 330}, "w")
-        .to("#reek-e-2",{duration: 1, morphSVG:"#swan-a", x: 230, y: 330}, "a")
-        .to("#reek-k",{duration: 1, morphSVG:"#swan-n", x: 240, y: 330}, "n")
+        .to("#reek-r",{duration: 1, morphSVG:"#swan-s", x: 210, y: 333}, "s")
+        .to("#reek-e-1",{duration: 1, morphSVG:"#swan-w", x: 220, y: 333}, "w")
+        .to("#reek-e-2",{duration: 1, morphSVG:"#swan-a", x: 230, y: 333}, "a")
+        .to("#reek-k",{duration: 1, morphSVG:"#swan-n", x: 240, y: 333}, "n")
         .to("#shit-s",{duration: 1, morphSVG:"#swan-s", x: 210, y: 140}, "s")
         .to("#shit-h",{duration: 1, morphSVG:"#swan-w", x: 220, y: 140}, "w")
         .to("#shit-i",{duration: 1, morphSVG:"#swan-a", x: 230, y: 140}, "a")
         .to("#shit-t",{duration: 1, morphSVG:"#swan-n", x: 240, y: 140}, "n")
-        .to("#scum-s",{duration: 1, morphSVG:"#swan-s", x: 210, y: -65}, "s")
-        .to("#scum-c",{duration: 1, morphSVG:"#swan-w", x: 220, y: -65}, "w")
-        .to("#scum-u",{duration: 1, morphSVG:"#swan-a", x: 230, y: -65}, "a")
-        .to("#scum-m",{duration: 1, morphSVG:"#swan-n", x: 240, y: -65}, "n");
+        .to("#scum-s",{duration: 1, morphSVG:"#swan-s", x: 210, y: -55}, "s")
+        .to("#scum-c",{duration: 1, morphSVG:"#swan-w", x: 220, y: -55}, "w")
+        .to("#scum-u",{duration: 1, morphSVG:"#swan-a", x: 230, y: -55}, "a")
+        .to("#scum-m",{duration: 1, morphSVG:"#swan-n", x: 240, y: -55}, "n");
     return quadTL;
 }
