fc7922b6a7119480f0754e75f67071624b986a59
js4679 <63320565+js4679@users.noreply.github.com>
2020-11-30 16:17:21 -0500

    color

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,16 +1,24 @@
-import {backgroundAnimation} from "./background.js"
-import {swansAnimation} from "./swans.js"
-import {quadAnimation} from "./quad.js"
-import {lockupAnimation} from "./lockup.js"
-
 import {gsap} from "gsap";
+import { GSDevTools } from "gsap/GSDevTools";
+
+
+import {backgroundAnimation} from "./background.js";
+// import {swansAnimation} from "./swans.js";
+import {quadAnimation} from "./quad.js";
+import {lockupAnimation} from "./lockup.js";
+
+
+
+gsap.registerPlugin(GSDevTools);
+
 
 const mainTL = gsap.timeline();
 
 mainTL.add(backgroundAnimation(), "play")
-        .add(swansAnimation())
+        // .add(swansAnimation())
         .add(quadAnimation(), "play")
-        .add(lockupAnimation());
+        .add(lockupAnimation(), "play2");
 
+GSDevTools.create();
     
 
